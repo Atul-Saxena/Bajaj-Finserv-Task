aadd740e6c98f44de09a6ca6dabcbda7c1f53a34
Atul Saxena <atulsaxena194.com>
2024-11-23 09:31:19 +0530

    ready to deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/bfhl': 'https://bajajfinserv-project-backend.vercel.app',
+      '/bfhl': 'https://bajaj-backend-kappa-blue.vercel.app',
     },
   },
   plugins: [react()],
